bdc876a888fa440773e6bcd4a2474d2dab335e6c
laurenh31 <128619296+laurenh31@users.noreply.github.com>
2023-05-31 09:27:44 -0700

    Its a start
    Imported the things

---
+++ README.md
@@ -0,0 +1,6 @@
+# Python IS
+# This is a testing folder for a learning community.
+
+Team Members:
+Ryan Magnuson
+Lauren Heywood
--- lauren.py
+++ lauren.py
@@ -1,3 +1,10 @@
 import pygame 
 import pickle 
 from os import path
+
+print("narrator: a figure emerges from the mist")
+print("   o   ")
+print("  /|\  ")
+print("  / \  ")
+print("figure: WELCOME! PLEASE DONT KILL ME")
+print("*dies*")
