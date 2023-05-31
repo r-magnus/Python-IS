8b9326b3062b60212cc4e7aec0d455a035fbc063
r-magnus <128619264+r-magnus@users.noreply.github.com>
2023-05-31 08:57:59 -0700

    refresh

--- ryan.py
+++ ryan.py
@@ -2,4 +2,22 @@ import pygame
 import pickle
 from os import path
 
-print("refresh")
+def store(item,file):
+  with open(file,"wb") as f:
+    pickle.dump(item,f)
+
+def collect(file):
+  if path.exists(file):
+    with open(file,"rb") as f:
+      return pickle.load(f)
+  return None
+
+FILE = "data.pkl"
+data = collect(FILE)
+if data is None:
+  data = {}
+
+# Edit data here
+# https://replit.com/@RyanMagnus/Serialization#main.py
+
+store(data, FILE)
